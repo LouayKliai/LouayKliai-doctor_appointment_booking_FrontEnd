487b4c8ad647e8f479c7fb61df5562cd7f597b20
LouayKliai <kliailouay@gmail.com>
2024-09-03 23:19:05 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './modules/patient/navbar/navbar.component';
+import { CardComponent } from "./modules/patient/card/card.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthRoutingModule } from './modules/auth/auth-routing.module';
+import { HomeComponent } from './shared/home/home.component';
+import { Component } from '@angular/core';
+import { NavbarComponent } from './modules/doctor/navbar/navbar.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'',component:HomeComponent}, 
+    {path:'/navbar',component:NavbarComponent},
+    {path:'/doctor',loadChildren:()=> import('./modules/doctor/doctor.module').then(m=>m.DoctorModule)}, 
+    {path:'auth',loadChildren: () => import('./modules/auth/auth-routing.module').then(m => m.AuthRoutingModule)}
+];
